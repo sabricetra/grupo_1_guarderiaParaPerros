287e7e3f9980bff1ecc6d274282d8a76c5537276
analiaferreyra <analiaferreyra0@gmail.com>
2022-04-30 12:43:28 -0300

    modificando rutas de css para que funcionen

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-
+// Importamos los distintos enrutadores
 const mainRouter = require("./src/routes/mainRouter.js")
 const productosRouter = require("./src/routes/productosRouter.js")
 const usersRouter = require("./src/routes/usersRouter.js")
@@ -13,11 +13,11 @@ app.set("view engine", "ejs")
 
 app.set('views', path.resolve(__dirname, './src/views'))
 
-app.listen(8000, ()=> console.log('Corriendo servidor en http://localhost:8000'))
 
+app.use("/users", usersRouter);
 
-app.use("/users", usersRouter)
-
-app.use("/productos", productosRouter)
+app.use("/productos", productosRouter);
 
 app.use("/", mainRouter)
+
+app.listen(8000, ()=> console.log('Corriendo servidor en http://localhost:8000'))
