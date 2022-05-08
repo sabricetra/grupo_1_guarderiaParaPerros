7776d609cca31bb8290e732dcaeac6ccdbdfa9dd
analiaferreyra <analiaferreyra0@gmail.com>
2022-05-08 16:10:59 -0300

    nombre de json corregido

--- src/controllers/productosController.js
+++ src/controllers/productosController.js
@@ -1,7 +1,7 @@
 const fs = require("fs")
 const path = require("path")
 
-const productosFilePath = path.join(__dirname , "../data/productosDataBase.json")
+const productosFilePath = path.join(__dirname , "../data/products.json")
 const productos = JSON.parse(fs.readFileSync(productosFilePath, "utf-8"))
 
 const productosController = {
