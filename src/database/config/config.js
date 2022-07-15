860e615710ff14dc36d2c3abaa170bb1af11458d
rodrigomartindc <rodrigomartindc@gmail.com>
2022-07-15 16:49:08 -0300

    css

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "campoy2011",
+    "password": "wireless",
     "database": "guarderiaMascotas",
     "host": "127.0.0.1",
     "dialect": "mysql"
