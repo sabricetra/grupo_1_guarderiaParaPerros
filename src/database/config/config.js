7bcd327195da5aad8bb32e19481438e2bb48810f
mariucampoy <mariucampoy@hotmail.com>
2022-07-15 15:27:21 -0300

    mejoras css

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "wireless",
+    "password": "campoy2011",
     "database": "guarderiaMascotas",
     "host": "127.0.0.1",
     "dialect": "mysql"
