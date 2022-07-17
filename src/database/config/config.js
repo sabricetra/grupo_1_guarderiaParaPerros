f38a1f5450703179862e2d4347207f66800e5799
rodrigomartindc <rodrigomartindc@gmail.com>
2022-07-17 20:41:18 -0300

    edits css header

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": null,
+    "password": "wireless",
     "database": "guarderiaMascotas",
     "host": "127.0.0.1",
     "dialect": "mysql"
